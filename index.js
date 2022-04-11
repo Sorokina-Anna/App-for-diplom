ef080863378c414e35f6a09b42de05afe9002214
Sorokina Anna <sorokina.ann.40@gmail.com>
2022-04-11 15:28:59 +0700

    Подключила бд

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require ('express')
+const mysql = require ('mysql2')
 
 const PORT = process.env.PORT || 3000
 
 const app = express ()
 
-app.listen (PORT, () => {
-    console.log ('Сервер запущен')
-})
+async function start () {
+    try {
+        const connection = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            database: "diplom",
+            password: "lbgkjvxtr2022"
+        })
+        app.listen (PORT, () => {
+            console.log ('Сервер запущен')
+        })
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start ()
+
